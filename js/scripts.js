1b5d4ca881424e6d60090a56e44297fa57da87cc
andy-dan <andrzej.daniluk@wp.pl>
2017-08-31 20:07:29 +0200

    first step

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+alert('Dotknij komutera a wybuchnie!!!'); //generuje komunkat
+console.log('Dotknij komutera a wybuchnie!!!');
+/* W pliku scripts.js zakomentuj linię z poleceniem alert dowolnym typem komentarza.
+Dodaj jeszcze dwa inne komentarze.
+Stwórz nowe repozytorium i wyślij tam swój projekt. Pamiętaj o udostępnieniu go mentorowi na Githubie. Link do projektu wstaw poniżej.*/
+// THE END
